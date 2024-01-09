5692b544f4d35da43034881ecf48808583c911c5
waelbedoui <waelbedoui10@gmail.com>
2024-01-09 09:25:59 +0100

    start

--- jobzen/server/index.js
+++ jobzen/server/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const db = require('./database/index.js')
+const JobOwnerRouter=require('./routes/jobOwnerRoutes.js')
 const PORT = 3000
 const app = express()
 const cors = require('cors')
@@ -9,6 +10,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(__dirname + '/../client/dist'))
+app.use('/api/jobOwner',JobOwnerRouter)
 
 
 app.listen(PORT, () => {
