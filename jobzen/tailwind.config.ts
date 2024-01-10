26ae51472e6ba6deea46ea2d068e0da7f78aa8df
Rahma <144438957+Rahma090@users.noreply.github.com>
2024-01-10 11:04:45 +0100

    Job detail Design (create - update - view)  (#3)
    * eliminate commented code
    
    * Jobdetails components

--- jobzen/tailwind.config.ts
+++ jobzen/tailwind.config.ts
@@ -8,10 +8,19 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      fontFamily:{
+      'lato':['Lato']
       },
     },
   },
