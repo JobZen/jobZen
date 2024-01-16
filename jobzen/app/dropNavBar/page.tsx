03e497a957a37454611e36a4528ef5cc8a7c7d20
waelbedoui <waelbedoui10@gmail.com>
2024-01-16 22:22:35 +0100

    7low

--- jobzen/app/dropNavBar/page.tsx
+++ jobzen/app/dropNavBar/page.tsx
@@ -1,35 +1,29 @@
 "use client"
-import React, { useState, useContext } from 'react';
+import { useState } from 'react';
 import Link from 'next/link';
-// import { DataContext } from '../context';
-interface Setting {
-  name: string;
-  href: string;
-}
+import { motion } from 'framer-motion';
 
-const settings: Setting[] = [
-  { name: 'Manage My Profile', href: '/' },
+const settings = [
+  { name: 'Account', href: '/' },
   { name: 'Messages', href: '/' },
-  { name: 'Logout', href: '/signup' }
+  { name: 'Logout', href: '/signup' },
 ];
 
 const DropNavbar = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  // const {user }:any=  useContext(DataContext)
-  const toggleDropdown = () => {
+
+  const handleToggle = () => {
     setIsDropdownOpen(!isDropdownOpen);
   };
 
   return (
-    <div>
-    <button
+    <div className="relative inline-flex items-center group">
+      <button
         id="avatarButton"
-        data-dropdown-toggle="userDropdown"
-        data-dropdown-placement="bottom"
-        className="w-14 h-14 rounded-full hover:drop-shadow-2xl cursor-pointer relative"
-        onClick={toggleDropdown}
+        className="rounded-full p-1 focus:outline-none"
+        onClick={() => handleToggle()}
       >
-        <img
+       <img
           className="w-10 h-10 rounded-full ring-2 ring-gray-300 dark:ring-gray-500"
           src="https://tecdn.b-cdn.net/img/new/avatars/2.jpg"
           // {user.image}
@@ -37,30 +31,44 @@ const DropNavbar = () => {
 
         />
       </button>
-      <div
-  id="userDropdown"
-  className={`absolute top-full w-80 h-80 left-0 -mr-[118px] ${isDropdownOpen ? 'bg-blur bg-rgba(128, 128, 128, 0.53)' : 'hidden'}`}>       
-          <div className='mr-[40px]'>
-          <div className="px-4 py-2 text-xl text-gray-900 dark:hover:text-gray-700 hover:text-bold">
-            {/* <div>{user.username}</div> */}
-            {/* <div className="font-medium truncate">{user.email}</div> */}
-          </div>
-          <ul className=" text-l bg-blur mr-4 bg-[#D3E8F8] text-black dark:text-gray-200" aria-labelledby="avatarButton">
-            {settings.map((setting) => (
-              <li key={setting.name}>
-                <Link href={setting.href} passHref>
-                  <span className="block px-4 py-2 text-black hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
-                    {setting.name}
-                  </span>
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
+      <motion.div
+  initial={{ opacity: 0, scaleY: 0 }}
+  animate={{ opacity: isDropdownOpen ? 1 : 0, scaleY: isDropdownOpen ? 1 : 0 }}
+  transition={{ duration: 0.3 }}
+  className="absolute origin-top-right z-10 w-[160px] h-[120px] left-[-77px] mt-[203px] -mr-[80px] bg-gradient-to-br from-[#275469] to-[#267296] divide-y divide-gray-200 rounded-3xl shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none hover:bg-transparent active:scale-95 transition-all duration-150 ease-linear"
+  aria-labelledby="options-menu"
+>
+  {settings.map((setting, index) => (
+    <Link href={setting.href} key={setting.name}>
+      <motion.a
+        className="block pl-6 pr-4 py-2 flex items-center text-white text-base font-medium hover:bg-transparent focus:outline-none"
+        whileHover={{ scale: 1.05, color: "#91c6EF", transition: { duration: 0.5 }}}
+      >
+        {index === 0 && (
+         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+         <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+       </svg>
+       
+        )}
+        {index === 1 && (
+         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+         <path stroke-linecap="round" stroke-linejoin="round" d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 0 1-.923 1.785A5.969 5.969 0 0 0 6 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337Z" />
+       </svg>
+       
+        )}
+        {index === 2 && (
+         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+         <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75" />
+       </svg>
+       
+        )}
+        {setting.name}
+      </motion.a>
+    </Link>
+  ))}
+</motion.div>
     </div>
   );
 };
 
 export default DropNavbar;
-
--- jobzen/server/database/config.js
+++ jobzen/server/database/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     host: "localhost",
     user: "root",
-    password: "realmadrid1902",
+    password: "root",
     database: "jobZen"
   };
