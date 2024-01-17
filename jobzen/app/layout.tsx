09fc2d42f25d21455ac07a5bcf995dcde9ac6032
Mohamed Chokri <144437680+MohamedChokri1@users.noreply.github.com>
2024-01-17 17:32:17 +0100

    hey (#32)
    * g
    
    * g
    
    * hey
    
    * hey
    
    * hey
    
    * hey
    
    * hey
    
    * hey
    
    * hey
    
    ---------
    
    Co-authored-by: Rahma <144438957+Rahma090@users.noreply.github.com>

--- jobzen/app/layout.tsx
+++ jobzen/app/layout.tsx
@@ -17,7 +17,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}
+      <body className={inter.className}>
+    {children}
       </body>
     </html>
   )
