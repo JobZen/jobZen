04caa4e59a2607a2366f749bdd76af15eaab6129
mohamedchokri1 <chokrimohamed170@gmail.com>
2024-01-22 10:21:01 +0100

    hey

--- jobzen/app/chat/[id]/page.tsx
+++ jobzen/app/chat/[id]/page.tsx
@@ -7,18 +7,43 @@ import { useState, useEffect } from "react";
 import io from "socket.io-client";
 import {useRouter}from 'next/navigation'
 
+interface Freelancer {
+  id: number
+  name: string
+  email: string
+  adress: string
+  phone: string
+  image: string
+  skill: string,
+  aboutMe: string,
+  experience: string,
+  jobtitle: string,
+  createdAt: string
+  updatedAt: string
+}
+interface  Jobowner {
+  id: number
+  name: string
+  email: string
+  adress: string
+  phone: number
+  image: string
+  rating: number
+  description: string,
+  createdAt: string
+  updatedAt:string
+}
 interface Message {
   id: number;
   body: string;
-  createdAt: Date;
+  createdAt: string;
   updatedAt: string;
   sender: number;
   reciever: number;
   him: boolean;
 }
 interface Person {
-  
-    
+
       id:number
       body: string
       createdAt : string
@@ -67,10 +92,14 @@ const Chat = () => {
   const [currentMessage, setCurrentMessage] = useState("");
   const [myMessage,setMyMessage]= useState<Person[] [] | []>([]);
   const [index , setIndex] = useState(0)
+  const [chatOwner,setchatOwner]= useState<Freelancer | Jobowner >()
+  const [chatReciever,setChatReciever]= useState<Freelancer | Jobowner >()
 
   const sortByCreatedAt = (a: Message, b: Message) =>
     new Date(a.createdAt) - new Date(b.createdAt);
 
+    const sortByCreatedAt1 = (a: Person, b: Person) =>
+     new Date(b[b.length-1].createdAt)-new Date(a[a.length-1].createdAt) 
 
 const route=useRouter()
 
@@ -108,23 +137,27 @@ const route=useRouter()
     let req1;
     let req2;
     let req3;
-   
+    let req4;
+    let req5;
     
     if ( role === "freelancer") {
       req1 = axios.get(`http://localhost:3000/freeMS/msg/${id}/${index}`);
       req2 = axios.get(`http://localhost:3000/jobMS/msg/${index}/${id}`);
       req3=axios.get(`http://localhost:3000/freeMS/msg/${id}`)
-     
+      req4 = axios.get(`http://localhost:3000/freelancer/${id}`)
+      req5 = axios.get (`http://localhost:3000/jobOwner//job-owner/${index}`)
     }
      else {
       req2 = axios.get(`http://localhost:3000/freeMS/msg/${index}/${id}`);
       req1 = axios.get(`http://localhost:3000/jobMS/msg/${id}/${index}`);
-      req3=axios.get(`http://localhost:3000/jobMS/msg/${id}`)
+      req3 =axios.get(`http://localhost:3000/jobMS/msg/${id}`)
+      req5 = axios.get(`http://localhost:3000/freelancer/${index}`)
+      req4 = axios.get (`http://localhost:3000/jobOwner//job-owner/${id}`)
     }
   
 
     axios
-      .all([req1, req2,req3])
+      .all([req1, req2,req3,req4,req5])
       .then(
         axios.spread((...res) => {
           res[1].data.forEach((element: { him: boolean }) => {
@@ -133,7 +166,10 @@ const route=useRouter()
           console.log(res[1].data);
           setSender(res[0].data);
           setReciever(res[1].data);
-          setMyMessage(res[2].data)
+          setMyMessage(res[2].data.sort(sortByCreatedAt1))
+          setchatOwner(res[3].data)
+          setChatReciever(res[4].data)
+
         })
       )
       .catch((error) => {
@@ -141,6 +177,7 @@ const route=useRouter()
       });
   }, [index]);
  
+  console.log(chatReciever,'dddd')
   const handleClassName = (freelancerId:number,jobownerId:number) => {
    
     const isOn=" items-center border-b-2 border-l-4 border-blue-400"
@@ -163,7 +200,78 @@ const handleRoute = (freelancerId:number,jobownerId:number) => {
 
 }
 
+function HandleDate(dateTimeString:string) {
 
+  const months = [
+    "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+];
+
+const dateTime = new Date(dateTimeString);
+
+const day = dateTime.getDate();
+const month = months[dateTime.getMonth()];
+const hours = dateTime.getHours();
+const minutes = dateTime.getMinutes();
+
+const formattedHours = hours % 12 === 0 ? 12 : hours % 12;
+
+const period = hours < 12 ? 'AM' : 'PM';
+
+const formattedDateTime = ` ${formattedHours}:${minutes < 10 ? '0' : ''}${minutes} ${period},${day} ${month}`;
+
+return formattedDateTime;
+
+}
+
+function HandleDateMe(dateTimeString:string) {
+
+  const months = [
+    "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+];
+
+const dateTime = new Date(dateTimeString);
+
+const day = dateTime.getDate();
+const month = months[dateTime.getMonth()];
+const hours = dateTime.getHours();
+const minutes = dateTime.getMinutes();
+
+const formattedHours = hours % 12 === 0 ? 12 : hours % 12;
+
+const period = hours < 12 ? 'AM' : 'PM';
+
+const formattedDateTime = ` ${day} ${month},${formattedHours}:${minutes < 10 ? '0' : ''}${minutes} ${period}`;
+
+return formattedDateTime;
+
+}
+const handleSend = () => {
+  if(role==="freelancer") {axios.post("http://localhost:3000/freeMS",{
+    sender: id,
+    reciever: index,
+    body: currentMessage
+  })
+  .then(()=> {console.log("the freelancer sended very cute message")})
+.catch((err)=>{console.log(err,'')})
+}
+  else
+   {axios.post("http://localhost:3000/jobMS",{
+    sender: id,
+    reciever: index,
+    body: currentMessage
+  })
+.then(()=> {console.log("the jobowner sended very cute message")})
+.catch((err)=>{console.log(err,'')})
+}
+
+const handleUsersSearch = () =>{
+
+}
+
+}
+ console.log("owner",chatOwner)
   return (
    
     <div className="container mx-auto shadow-lg rounded-lg">
@@ -180,7 +288,7 @@ const handleRoute = (freelancerId:number,jobownerId:number) => {
           />
         </div>
         <div className="h-12 w-12 p-2 bg-yellow-500 rounded-full text-white font-semibold flex items-center justify-center">
-          RA
+          {chatOwner?.name.slice(0,2).toUpperCase()}
         </div>
       </div>
     
@@ -192,6 +300,7 @@ const handleRoute = (freelancerId:number,jobownerId:number) => {
             <input
               type="text"
               placeholder="search chatting"
+          
               className="py-2 px-2 border-2 border-gray-200 rounded-2xl w-full"
             />
           </div>
@@ -201,7 +310,7 @@ const handleRoute = (freelancerId:number,jobownerId:number) => {
            <div key={i} className={handleClassName(el[el.length-1].freelancer.id,el[el.length-1].jobowner.id)+" flex flex-row py-4 px-2 cursor-pointer"} onClick={()=>{handleRoute(el[el.length-1].freelancer.id,el[el.length-1].jobowner.id)}}>
             <div className="w-1/4">
               <img
-                src="https://source.unsplash.com/_7LbC5J-jw4/600x600"
+                src= {role==="freelancer"? el[el.length-1].jobowner.image:el[el.length-1].freelancer.image}
                 className="object-cover h-12 w-12 rounded-full"
                 alt=""
               />
@@ -213,12 +322,12 @@ const handleRoute = (freelancerId:number,jobownerId:number) => {
                el[el.length-1].freelancer.name}</div>
               <span className="text-gray-500">{role==="freelancer"?
               el[el.length-1].freelancer.updatedAt?
-               "him :"+ el[el.length-1].body
+                el[el.length-1].body
                 :
                "you :"+ el[el.length-1].body
                :
                el[el.length-1].jobowner.updatedAt?
-                "him :"+ el[el.length-1].body
+                 el[el.length-1].body
                 :
                 "you :"+ el[el.length-1].body}</span>
             </div>
@@ -234,19 +343,23 @@ const handleRoute = (freelancerId:number,jobownerId:number) => {
           <div className="flex flex-col mt-5 overflow-y-auto max-h-[481px]">
           {sorted.map((message ,i)=>(<div key={i}> {message.him?  <div className="flex justify-start mb-4">
               <img
-                src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
+                src={chatReciever?.image}
                 className="object-cover h-8 w-8 rounded-full"
                 alt=""
               />
               <div className="ml-2 py-2 px-4 bg-gray-400 rounded-br-3xl rounded-tr-3xl rounded-tl-xl text-white">
                {message.body}
               </div>
-            </div>: <div className="flex justify-end mb-4">
+              <span className="text-sm ml-[10px] font-normal text-gray-500 dark:text-gray-400">{HandleDate(message.createdAt)}</span>
+            </div>:
+             <div className="flex justify-end mb-4">
+                  <span className="text-sm mr-[10px] font-normal text-gray-500 dark:text-gray-400">{HandleDateMe(message.createdAt)}</span>
               <div className="mr-2 py-2 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white">
                {message.body}
               </div>
+          
               <img
-                src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
+                src={chatOwner?.image}
                 className="object-cover h-8 w-8 rounded-full"
                 alt=""
               />
@@ -262,26 +375,29 @@ const handleRoute = (freelancerId:number,jobownerId:number) => {
             <input
               className=" bg-gray-300 py-3 px-3 rounded-xl w-[44rem]"
               type="text"
+              onChange={(e)=>{setCurrentMessage(e.target.value)}}
               placeholder="type your message here..."
             />
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-4 border border-blue-700 rounded-xl">
-  Send
-</button>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-4 border border-blue-700 rounded-xl"
+            onClick={()=>{handleSend()}}
+            >
+            Send
+            </button>
           </div>
         </div>
         {/* <!-- end message --> */}
         <div className="w-2/5 border-l-2 px-5">
           <div className="flex flex-col">
-            <div className="font-semibold text-xl py-4">Mern Stack Group</div>
+            <div className="font-semibold text-xl py-4">You Chating With {chatReciever?.name}</div>
             <img
               src="https://source.unsplash.com/L2cxSuKWbpo/600x600"
               className="object-cover rounded-xl h-64"
               alt=""
             />
-            <div className="font-semibold py-4">Created 22 Sep 2021</div>
-            <div className="font-light">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt,
-              perspiciatis!
+            <div className="font-semibold py-4">Created 22 Jan 2024</div>
+            <div className="font-light mb-[20px]">
+           {role==="freelance"? `Welcome ${chatOwner?.name}! We're thrilled to have you join our community of talented freelancers. Here, you'll find a wide range of exciting job opportunities that match your skills and expertise.`:` We're thrilled to have you on board ${chatOwner?.name}! Your journey to finding the perfect freelancers begins here.
+            Our platform is designed to connect you with skilled professionals who can bring your projects to life.`}
             </div>
           </div>
         </div>
