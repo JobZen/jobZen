5f004bca8859f0d89e52cce16c72ac2d9c76157f
Rahma <144438957+Rahma090@users.noreply.github.com>
2024-01-24 12:00:49 +0100

    admin & schema update  (#59)
    * admin inbox
    
    * none

--- jobzen/server/routes/contactRouter.js
+++ jobzen/server/routes/contactRouter.js
@@ -3,5 +3,7 @@ const Contact=require('../controllers/contactController')
 
 router.post('/add',Contact.add)
 router.get('/get',Contact.get)
+router.delete('/delete/:id', Contact.delete);
+router.post('/addReply', Contact.addReply);
 
 module.exports =router
