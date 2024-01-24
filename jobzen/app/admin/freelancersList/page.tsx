0b2f4533861c1a3111cb57339d0435567fe36bf3
fares chaouali <144439333+fares1902@users.noreply.github.com>
2024-01-24 16:23:34 +0100

    fares (#61)

--- jobzen/app/admin/freelancersList/page.tsx
+++ jobzen/app/admin/freelancersList/page.tsx
@@ -5,8 +5,11 @@ import Link from 'next/link';
 import SideNavBar from '../sideNavBar/page';
 import { MdKeyboardArrowUp } from 'react-icons/md';
 import Search from '../../search/page'
+import Freelancer from '@/app/freelancer/[id]/page';
+import Popup from 'reactjs-popup'; 
+import 'reactjs-popup/dist/index.css'; 
 
-interface Freelancer{
+interface Freelancer {
   id:number,
   name:string,
   email:string,
@@ -24,8 +27,28 @@ const FreelancersList = () => {
 const [freelancers, setFreelancers] = useState<Freelancer[]>([])
 const [freelancerCount,setFreelancerCount]=useState<number | undefined>()
 
+const [refresh,setRefresh]=useState<boolean>(false)
+// const [showConf,setShowConf]=useState<boolean>(false)
+
 const containerRef = useRef<HTMLDivElement>(null);
 const [rowCount, setRowCount] = useState<number>(0);
+
+
+const [freelancer, setfreelancer] = React.useState<Freelancer[]>([]);
+
+
+
+
+const handleDelete = async (id: number) => {
+  try {
+    await axios.delete(`http://localhost:3000/freelancer/${id}`);
+    setRefresh(!refresh)
+
+  } catch (error) {
+    console.error('Error deleting freelancer:', error);
+  }
+
+};
 
 useEffect(()=>{  
 const getAllFreelancer = async () => {
@@ -39,7 +62,7 @@ const getAllFreelancer = async () => {
   }
 };
 getAllFreelancer()
-},[])
+},[refresh])
 
 useEffect(() => {
   if (rowCount >= 10 && containerRef.current) {
@@ -52,11 +75,27 @@ const handleScrollUp = () => {
   setRowCount((prevCount) => prevCount + 1);
 };
 
+
+
+
+  const handleCancelClick = () => {
+  
+    return;
+  }
+  
+
+
+  
+
+
+
+
+
 return (
     <>
 <div className="p-4 sm:ml-64" >
 <SideNavBar />
-{/* First row with three columns */}
+
     <div className=" gap-4 mb-10 mt-10" ref={containerRef}>
       <div className="flex flex-col items-center justify-center h-24 rounded">
       {rowCount >= 10 && (
@@ -187,11 +226,59 @@ return (
             </td>
             <td className="px-6 py-4">
               {/* <!-- Modal toggle --> */}
+              <div>
               <a
               href="#"
               type="button"
               className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 inline-flex items-center text-red-700 hover:text-white border border-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900">
-              <span className="ml-2">Delete Freelancer</span></a>
+             
+             <Popup
+    trigger={<button className="ml-2"> Delete freelancer </button>}
+    modal
+    nested
+  >
+      {close => (
+     <div className="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+
+   
+
+  <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+
+  <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+    <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+      
+       
+      
+      <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+        <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+          <div className="sm:flex sm:items-start">
+            <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+              <svg className="h-6 w-6 text-red-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
+              </svg>
+            </div>
+            <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+              <h3 className="text-base font-semibold leading-6 text-gray-900" id="modal-title">Delete account</h3>
+              <div className="mt-2">
+                <p className="text-sm text-gray-500">Are you certain you wish to delete your account? All of your data will be permanently erased. This action is irreversible.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+          <button type="button" className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto" onClick={() => handleDelete(freelancer.id)}>Delete</button>
+          <button type="button" className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto" onClick={() => {
+              console.log('modal closed ');
+              close();
+            }} >Cancel</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div> )}
+      </Popup>
+             </a>
+              </div>
             </td>
           </tr>
           ))}
@@ -199,6 +286,8 @@ return (
       </table>
     </div>
   </div>
+        
+  
 </>
 
 );
--- jobzen/server/database/config.js
+++ jobzen/server/database/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     host: "localhost",
     user: "root",
-    password: "kamelsamira",
+    password: "realmadrid1902",
     database: "jobzen"
   };
