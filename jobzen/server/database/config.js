e36bc72758fd65c109ef5557898b4777e56388bf
Rahma090 <rahma.aliani@gmail.com>
2024-01-24 09:13:46 +0100

    none

--- jobzen/server/database/config.js
+++ jobzen/server/database/config.js
@@ -1,10 +1,6 @@
 module.exports = {
     host: "localhost",
     user: "root",
-<<<<<<< HEAD
     password: "Rahma1990@",
-=======
-    password: "root",
->>>>>>> ae100c0a20346d1f813aa726ba1f9d09d89df09f
     database: "jobzen"
   };
