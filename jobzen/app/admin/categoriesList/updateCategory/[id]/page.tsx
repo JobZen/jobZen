b53630505cc43348c4f33c60cf5abeb6c082c334
Rahma090 <rahma.aliani@gmail.com>
2024-01-29 18:38:26 +0100

    eliminate checkbox

--- jobzen/app/admin/categoriesList/updateCategory/[id]/page.tsx
+++ jobzen/app/admin/categoriesList/updateCategory/[id]/page.tsx
@@ -126,20 +126,6 @@ return (
     <table className="w-full text-sm text-left rtl:text-right  border-[#267296] border-b-4 border-4 bg-[#267296]">
       <thead className="text-xs text-white border-[#267296] uppercase border-b-4">
         <tr>
-          <th scope="col"className="p-4 bg-">
-            <div className="flex items-center ">
-              <input
-                id="checkbox-all-search"
-                type="checkbox"
-                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-              />
-              <label
-                htmlFor="checkbox-all-search"
-                className="sr-only ">
-                checkbox
-              </label>
-            </div>
-          </th>
           <th
             scope="col"
             className="px-6 py-3 bg-grey-500"
@@ -158,21 +144,6 @@ return (
         <tr
           className="bg-white border-gray-400 hover:bg-[#91C7EF] group transition duration-200 "
         >
-          <td className="w-4 p-4">
-            <div className="flex items-center">
-              <input
-                id="checkbox-table-search-1"
-                type="checkbox"
-                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-              />
-              <label
-                htmlFor="checkbox-table-search-1"
-                className="sr-only"
-              >
-                checkbox
-              </label>
-            </div>
-          </td>
           <th
             scope="row"
             className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap "
